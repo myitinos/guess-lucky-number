51f0444b4fec9567efaf82efa93dd810cd49f30c
myitinos <leonaedo240898@gmail.com>
2024-04-21 18:06:34 +0800

    update card hint pos query docs

--- app/router/card.py
+++ app/router/card.py
@@ -64,7 +64,7 @@ def view_card_hint(
         app.model.card.Card,
         fastapi.Depends(get_single_card),
     ],
-    pos: int = fastapi.Query(ge=0, lt=app.model.card.NUMBER_LEN),
+    pos: int = fastapi.Query(ge=0, le=app.model.card.NUMBER_LEN - 1),
 ) -> app.model.card.CardHint:
     """Get a hint for a card that you already drawn,
     you only have limited number of hint!
